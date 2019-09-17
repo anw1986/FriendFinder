c266f9e132997ffe9f85bd74355c73e89b47a472
awaheed <awaheed@utexas.edu>
2019-09-17 14:44:50 -0500

    multiple match and modal list now working

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,19 +3,19 @@
 var friends = [{
         indexPosition: "",
         name: "Ahmed",
-        image: "./../../images/ahmed.jpg",
+        image: "/images/ahmed.jpg",
         score: [1, 2, 3, 4, 5, 1, 2, 3, 4, 1]
     },
     {
         indexPosition: "",
         name: "Mickey",
-        image: "./../../images/mickey.jpg",
+        image: "/images/mickey.jpg",
         score: [3, 4, 3, 3, 3, 3, 3, 3, 3, 1]
     },
     {
         indexPosition: "",
         name: "ryan",
-        image: "./../../images/ryan.jpg",
+        image: "/images/ryan.jpg",
         score: [1, 2, 3, 1, 2, 3, 5, 1, 5, 1]
     }
 ];
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,6 +11,18 @@ module.exports = function(app){
     res.sendFile(path.join(__dirname, "./../public/survey.html"));
   });
   
+  app.get("/images/ahmed.jpg",function(req,res){
+    res.sendFile(path.join(__dirname, "../../public/images/ahmed.jpg"));
+  })  
+
+  app.get("/images/mickey.jpg",function(req,res){
+    res.sendFile(path.join(__dirname, "../../public/images/mickey.jpg"));
+  })  
+
+  app.get("/images/ryan.jpg",function(req,res){
+    res.sendFile(path.join(__dirname, "../../public/images/ryan.jpg"));
+  })  
+
   app.get("*",function(req,res){
     res.sendFile(path.join(__dirname, "./../public/home.html"));
   })
